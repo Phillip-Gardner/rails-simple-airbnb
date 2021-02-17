be032da5f3579a626ee330bcd36d2d0fa8ea4b54
Phillip Gardner <pbg.gardner@gmail.com>
2021-02-17 17:31:09 +0000

    update

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,23 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show]
+
   def index
     @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+  end
+
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import 'bootstrap';
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
+
+// app/javascript/packs/application.js
+import 'bootstrap';
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
