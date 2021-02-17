32c0a55fa13a222eeff760cef28714689a0ed406
Phillip Gardner <pbg.gardner@gmail.com>
2021-02-17 16:28:28 +0000

    home page complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+15.times do
+  flat = Flat.new(
+    name: Faker::Mountain.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::TvShows::FamilyGuy.quote,
+    price_per_night: rand(0..1000),
+    number_of_guests: rand(0..25)
+  )
+  flat.save!
+end
